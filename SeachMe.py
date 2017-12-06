5b28e06ab0a6544bda22741c34f99e9eae66eb36
Yohei Ema <yameholo@YoheinoMacBook-Air.local>
2017-12-06 15:45:06 +0900

    first commit

---
+++ SeachMe.py
@@ -0,0 +1,74 @@
+from subprocess import check_call
+from os import path
+
+from sqlite3 import connect
+
+from janome.tokenizer import Tokenizer
+
+raw_database_file = "History.db"
+database_file = "my_history.db"
+
+
+def setup_history_database():
+    # copy safari history database to working directory
+    # for safety work
+    check_call(["cp", path.expanduser("~/Library/Safari/") + raw_database_file, "./"])
+
+
+def open_sql(filename, f):
+    # open (or create) database
+    connector = connect(filename)
+    cursor = connector.cursor()
+
+    # execute sql commands
+    result = f(cursor)
+
+    if result is None:
+        # updated this database if return value of f() is None
+        connector.commit()
+    # else: got data from this database if return value of f() is not None
+
+    cursor.close()
+    connector.close()
+
+    # result value is None or data
+    return result
+
+
+def execute_sql_command(command=""):
+    def f(cursor):
+        cursor.execute(command)
+        # get selected data if this command use select
+        if command[:6] == "select":
+            return cursor.fetchall()
+        return None
+    return f
+
+
+def get_prepared_data():
+    cmd = "select visit_time,title from history_visits where title != '' group by title order by id"
+    result = open_sql(raw_database_file, execute_sql_command(cmd))
+    # add id
+    for i in range(len(result)):
+        result[i] = (i, result[i][0], result[i][1])
+    return result
+
+
+def hoge():
+    t = Tokenizer()
+    for token in t.tokenize(u'すもももももももものうち'):
+        partOfSpeech = token.part_of_speech.split(',')[0]
+        if partOfSpeech == u'名詞':
+            print(token.surface)
+
+
+def main():
+    setup_history_database()
+    r = get_prepared_data()
+    for t in r:
+        print(t)
+    hoge()
+
+
+if __name__ == '__main__':
+    main()
